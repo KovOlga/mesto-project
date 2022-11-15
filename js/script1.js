e49f960c0f83a4dab660f7745102c5c4c713d303
KovOlga <switch.to.olga@yandex.ru>
2022-11-15 22:05:21 +0300

    typo

--- js/script1.js
+++ js/script1.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const editButton = document.querySelector('.profile__edit-button');
 editButton.addEventListener('click', function () {
-    openPopup(popupProfile);
+    openPopup(popupProfile); jj
 });
 editButton.addEventListener('click', function () {
     popupProfile.classList.add('popupProfile_opened');
