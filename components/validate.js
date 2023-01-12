a2921680956921c1796dcb359fb2edaa980a9012
KovOlga <switch.to.olga@yandex.ru>
2023-01-12 21:57:08 +0300

    added validate.js and devided functions

---
+++ components/validate.js
@@ -0,0 +1,65 @@
+function showInputError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+  inputElement.classList.add("popup__item_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("popup__item-error_active");
+}
+
+function hideInputError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("popup__item_type_error");
+  errorElement.classList.remove("popup__item-error_active");
+  errorElement.textContent = "";
+}
+
+function isValid(formElement, inputElement) {
+  if (inputElement.value.length === 0) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+  } else {
+    inputElement.setCustomValidity("");
+  }
+
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+function setEventListeners(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll(".popup__item"));
+  const buttonElement = formElement.querySelector(".popup__form-save");
+  setSubmitBtnState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      isValid(formElement, inputElement);
+      setSubmitBtnState(inputList, buttonElement);
+    });
+  });
+}
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll(".popup__form"));
+
+  formList.forEach((formElement) => {
+    setEventListeners(formElement);
+  });
+}
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+function setSubmitBtnState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.disabled = true;
+    buttonElement.classList.add("popup__form-save_disabled");
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove("popup__form-save_disabled");
+  }
+}
