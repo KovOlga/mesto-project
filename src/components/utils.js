6e6a64d4af873541caf00e931d3378b20971a4f9
KovOlga <switch.to.olga@yandex.ru>
2023-01-19 16:38:54 +0300

    fixed submitBtnDisabling on Reopen

--- src/components/utils.js
+++ src/components/utils.js
@@ -16,13 +16,20 @@ function resetErrorOnOpen(formElement) {
   });
 }
 
+function disableSubmitBtnOnReopen(btnElement) {
+  btnElement.setAttribute("disabled", true);
+  btnElement.classList.add("form__submit-btn_disabled");
+}
+
 function openPopup(openElement) {
   if (openElement.classList.contains("popup_profile")) {
     resetErrorOnOpen(formProfile);
+    disableSubmitBtnOnReopen(formProfile.elements.submitProfile);
   }
   if (openElement.classList.contains("popup_new-place")) {
     formPlace.reset();
     resetErrorOnOpen(formPlace);
+    disableSubmitBtnOnReopen(formPlace.elements.submitPlace);
   }
   openElement.classList.add("popup_opened");
 }
