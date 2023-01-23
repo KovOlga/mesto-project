1c798b25f53c798173d717bc1ce49a5fd744d3f5
KovOlga <switch.to.olga@yandex.ru>
2023-01-23 17:18:54 +0300

    added universal getResponse for fetch

--- src/components/api.js
+++ src/components/api.js
@@ -1,9 +1,16 @@
+const getResponse = (res) => {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Ошибка${res.statusText}`);
+};
+
 const getCardData = () => {
   return fetch("https://nomoreparties.co/v1/plus-cohort-19/cards", {
     headers: {
       authorization: "858c7cb6-e0c3-4a29-bdeb-66efeccdb118",
     },
-  }).then((res) => res.json());
+  }).then(getResponse);
 };
 
 export { getCardData };
