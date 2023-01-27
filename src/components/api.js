0ffde2aa4c126923a7b1fc3cd04e229225cb35e2
KovOlga <switch.to.olga@yandex.ru>
2023-01-27 15:08:55 +0300

    fixed catch

--- src/components/api.js
+++ src/components/api.js
@@ -10,7 +10,7 @@ function getResponse(res) {
   if (res.ok) {
     return res.json();
   }
-  return Promise.reject(`Ошибка${res.statusText}`);
+  return Promise.reject(`Ошибка: ${res.status}`);
 }
 
 const getUserData = () => {
