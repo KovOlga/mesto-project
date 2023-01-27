b3978772820a4fb0efade0d7c23e9d548614772e
KovOlga <switch.to.olga@yandex.ru>
2023-01-27 15:36:26 +0300

    rewrite card js

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,6 @@
-import { popupImage } from "./data.js";
-import { openPopup } from "./modal.js";
+import { popupImage, popupAgreeDelete } from "./data.js";
+import { openPopup, closePopup } from "./modal.js";
+import { deleteCard, putLike, deleteLike } from "./api.js";
 
 const popupImagePicture = popupImage.querySelector(".popup__image");
 const popupImageCaption = popupImage.querySelector(".popup__caption");
@@ -7,48 +8,132 @@ const photoElementsGallery = document.querySelector(".photo-elements__list");
 const photoCardTemplateContent = document.querySelector(
   "#photo-cards-template"
 ).content;
+const btnAgreeDelete = document.querySelector(".popup__btn-agree");
+const btnLikeActiveClass = "photo-elements__like-button_active";
+const likeCounterDisabledClass = "photo-elements__like-container_disabled";
 
-const renderCard = function () {
-  return {
-    createCard: function (card) {
-      const photoCardElement = photoCardTemplateContent.cloneNode(true);
-      const photoElementTitle = photoCardElement.querySelector(
-        ".photo-elements__title"
-      );
-      photoElementTitle.textContent = card.name;
-      const photoElementImage = photoCardElement.querySelector(
-        ".photo-elements__image"
-      );
-      photoElementImage.src = card.link;
-      photoElementImage.alt = card.name;
-      photoCardElement
-        .querySelector(".photo-elements__like-button")
-        .addEventListener("click", (evt) => {
-          evt.target.classList.toggle("photo-elements__like-button_active");
+let currentUserId;
+
+function setCurrentUserId(userId) {
+  currentUserId = userId;
+}
+
+function renderCard(cardData) {
+  photoElementsGallery.prepend(createCardElement(cardData));
+}
+
+function createCardElement(cardData) {
+  const photoCardElement = photoCardTemplateContent.cloneNode(true);
+  const photoElementTitle = photoCardElement.querySelector(
+    ".photo-elements__title"
+  );
+  photoElementTitle.textContent = cardData.name;
+  const photoElementImage = photoCardElement.querySelector(
+    ".photo-elements__image"
+  );
+  photoElementImage.src = cardData.link;
+  photoElementImage.alt = cardData.name;
+
+  const counterLike = photoCardElement.querySelector(
+    ".photo-elements__like-counter"
+  );
+  const btnLike = photoCardElement.querySelector(
+    ".photo-elements__like-button"
+  );
+  const btnDeletePhotoElement = photoCardElement.querySelector(
+    ".photo-elements__bin-button"
+  );
+
+  let isLike;
+
+  //закрасили, если уже есть лайк юзера
+  if (cardData.likes) {
+    cardData.likes.forEach((likeElementOwner) => {
+      if (likeElementOwner._id === currentUserId) {
+        btnLike.classList.add(btnLikeActiveClass);
+      }
+    });
+  }
+
+  //отрисовали количество лайков
+  if (cardData.likes.length === 0) {
+    counterLike.classList.add(likeCounterDisabledClass);
+  } else {
+    counterLike.textContent = cardData.likes.length.toString();
+  }
+
+  function processLike() {
+    const likeState = cardData.likes.some((likeData) => {
+      return likeData._id === currentUserId;
+    });
+    isLike = likeState;
+  }
+
+  btnLike.addEventListener("click", (evt) => {
+    processLike();
+    if (isLike) {
+      return deleteLike(cardData._id)
+        .then((res) => {
+          console.log("remove like");
+          evt.target.classList.toggle(btnLikeActiveClass);
+          if (res.likes.length === 0) {
+            counterLike.classList.add(likeCounterDisabledClass);
+          } else {
+            counterLike.classList.remove(likeCounterDisabledClass);
+            counterLike.textContent = res.likes.length.toString();
+          }
+          cardData = res;
+        })
+        .catch((err) => {
+          console.log(
+            `Ошибка при отправке данных о лайке на сервер: ${err.message}`
+          );
         });
-      const photoElementDeleteBtn = photoCardElement.querySelector(
-        ".photo-elements__bin-button"
-      );
-      photoElementDeleteBtn.addEventListener("click", () => {
-        const dlt = photoElementDeleteBtn.closest(".photo-elements__item");
-        dlt.remove();
+    } else {
+      return putLike(cardData._id)
+        .then((res) => {
+          console.log("add like");
+          evt.target.classList.toggle(btnLikeActiveClass);
+          counterLike.classList.remove(likeCounterDisabledClass);
+          counterLike.textContent = res.likes.length.toString();
+          cardData = res;
+        })
+        .catch((err) => {
+          console.log(
+            `Ошибка при отправке данных о лайке на сервер: ${err.message}`
+          );
+        });
+    }
+  });
+
+  //отрисовываем иконку корзины и вешаем слушатель открытия попапа/удаления, если айди юзера совпадает
+  if (currentUserId === cardData.owner._id) {
+    btnDeletePhotoElement.addEventListener("click", () => {
+      openPopup(popupAgreeDelete);
+      btnAgreeDelete.addEventListener("click", () => {
+        deleteCard(cardData._id)
+          .then(() => {
+            closePopup(popupAgreeDelete);
+            btnDeletePhotoElement.closest(".photo-elements__item").remove();
+          })
+          .catch((err) => {
+            console.log(`Ошибка при удалении карточки: ${err.message}`);
+          });
       });
+    });
+  } else {
+    btnDeletePhotoElement.remove();
+  }
 
-      photoElementImage.addEventListener("click", () => {
-        popupImagePicture.src = photoElementImage.src;
-        popupImagePicture.alt = photoElementImage.alt;
-        popupImageCaption.textContent = photoElementTitle.textContent;
+  photoElementImage.addEventListener("click", () => {
+    popupImagePicture.src = photoElementImage.src;
+    popupImagePicture.alt = photoElementImage.alt;
+    popupImageCaption.textContent = photoElementTitle.textContent;
 
-        openPopup(popupImage);
-      });
+    openPopup(popupImage);
+  });
 
-      return photoCardElement;
-    },
+  return photoCardElement;
+}
 
-    addCard: function (card) {
-      photoElementsGallery.prepend(this.createCard(card));
-    },
-  };
-};
-
-export { renderCard };
+export { renderCard, setCurrentUserId };
