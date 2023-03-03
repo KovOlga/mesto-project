6f3c8f0242ce2bc0e394b7620501a2fd28aef258
KovOlga <switch.to.olga@yandex.ru>
2023-03-03 11:38:53 +0300

    fixed PopupWithImage after first review

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,10 +1,15 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
-  constructor({ popup, popupImagePictureSelector, popupImageCaptionSelector }) {
-    super(popup);
-    this.image = document.querySelector(popupImagePictureSelector);
-    this.caption = document.querySelector(popupImageCaptionSelector);
+  constructor({
+    popupSelector,
+    popupImagePictureSelector,
+    popupImageCaptionSelector,
+  }) {
+    super(popupSelector);
+    this.popup = document.querySelector(popupSelector);
+    this.image = this.popup.querySelector(popupImagePictureSelector);
+    this.caption = this.popup.querySelector(popupImageCaptionSelector);
   }
 
   open(imageName, imageLink) {
